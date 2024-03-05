219e9d6e9c259a5f8ca1e55e60ede36df136242e
Jacob  Rethmeier <142352434+jakereth@users.noreply.github.com>
2024-03-05 09:11:26 -0800

    Update main.py

--- 17. Quiz Game/main.py
+++ 17. Quiz Game/main.py
@@ -1,9 +1,12 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import Quiz
+#import all files data is being used from
 
+#create a list to store all questions
 questions = []
 
+#loop through data in question file seperating questions from answers
 for entry in question_data:
     # Extract text and answer from the dictionary
     text = entry["text"]
@@ -15,6 +18,7 @@ for entry in question_data:
 
 quiz = Quiz(questions)
 
+#loop while there is still questions and while user doesnt want to exit
 while quiz.still_has_questions():
     cont = input("Would you like to continue? Y for yes\nN for no").lower()
     if cont == 'n':
