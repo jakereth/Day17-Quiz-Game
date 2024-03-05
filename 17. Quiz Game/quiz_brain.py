e5ea058f6927d13dd50adc46c967bf8c4b35f092
Jacob  Rethmeier <142352434+jakereth@users.noreply.github.com>
2024-03-05 09:15:05 -0800

    Update quiz_brain.py

--- 17. Quiz Game/quiz_brain.py
+++ 17. Quiz Game/quiz_brain.py
@@ -1,19 +1,22 @@
 class Quiz:
-
+    #initialize the quiz with base values
     def __init__(self, qList):
         self.questionNum = 0
         self.questionList = qList
         self.score = 0
 
+        #function to display question number and current question
     def nextQuestion(self):
          currentQuestion = self.questionList[self.questionNum]
          self.questionNum += 1
          user_answer = input(f"Q.{self.questionNum}: {currentQuestion} (True/False): ")
          self.check_answer(user_answer, currentQuestion)
 
+        #function to check if there is still questions 
     def still_has_questions(self):
         return self.questionNum < len(self.questionList)
 
+        #function to compare user answer to correct answer
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             print("You got it right!")
