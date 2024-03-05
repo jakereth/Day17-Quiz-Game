2c6adbbb151127a88d6b92ed6f3d83980c2d926f
Jacob  Rethmeier <142352434+jakereth@users.noreply.github.com>
2024-03-05 09:08:02 -0800

    Delete 17. Quiz Game directory

--- 17. Quiz Game/question_model.py
+++
@@ -1,7 +0,0 @@
-from data import question_data
-
-class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-
--- 17. Quiz Game/quiz_brain.py
+++
@@ -1,8 +0,0 @@
-class Quiz:
-
-    def __init__(self, qList):
-        self.questionNum = 0
-        self.questionList = 
-
-    def nextQuestion(self,questionNum):
-         
